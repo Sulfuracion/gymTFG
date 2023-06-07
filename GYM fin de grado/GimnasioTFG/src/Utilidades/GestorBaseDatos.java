7d2eff5ea739bee5c9867e772c79bb25c5b83b7d
Sulfuracion <116578887+Sulfuracion@users.noreply.github.com>
2023-06-07 16:17:52 -0500

    07/06/23`
    Se añaden dos botones y se crear el código para que sean funcionales

--- GYM fin de grado/GimnasioTFG/src/Utilidades/GestorBaseDatos.java
+++ GYM fin de grado/GimnasioTFG/src/Utilidades/GestorBaseDatos.java
@@ -1,32 +1,82 @@
 package Utilidades;
 
-import Utilidades.Constantes;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class GestorBaseDatos {
 
     private Constantes constantes;
-    
+    private Connection conexion;
+
     public void conectarBBDD() {
-        Connection conex = null;
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            conex = DriverManager.getConnection(constantes.getURL_BBDD(), constantes.getUSUARIO_BBDD(), constantes.getCONTRASEÑA_BBDD());
-            Statement a = conex.createStatement();
-            System.out.println("Funsiona");
-            
+            conexion = DriverManager.getConnection(constantes.getURL_BBDD(), constantes.getUSUARIO_BBDD(), constantes.getCONTRASEÑA_BBDD());
+            System.out.println("Conexión exitosa a la base de datos.");
+        } catch (SQLException e) {
+            System.out.println("Error al conectar a la base de datos.");
+            e.printStackTrace();
         }
-        catch (SQLException e){
-            System.out.println("No se ha podido añadir los datos a la base de datos");
+    }
+
+    public Connection getConexion() {
+        return conexion;
+    }
+
+    public void cerrarConexion() {
+        if (conexion != null) {
+            try {
+                conexion.close();
+                System.out.println("Conexión cerrada.");
+            } catch (SQLException e) {
+                System.out.println("Error al cerrar la conexión.");
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void verRutinas() {
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            statement = conexion.createStatement();
+            String sql = "SELECT * FROM Rutinas";
+            resultSet = statement.executeQuery(sql);
+
+            // Procesar el resultado de la consulta
+            while (resultSet.next()) {
+                int idRutina = resultSet.getInt("ID_rutina");
+                int idUsuario = resultSet.getInt("ID_usuario");
+                String nombreRutina = resultSet.getString("NombreRutina");
+
+                System.out.println("ID Rutina: " + idRutina);
+                System.out.println("ID Usuario: " + idUsuario);
+                System.out.println("Nombre Rutina: " + nombreRutina);
+                System.out.println("--------------------------------------");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al ejecutar la consulta SQL");
             e.printStackTrace();
-        } 
-        catch (Exception e) {
-            System.out.println("Error en la aplicación.");
-            e.printStackTrace();
+        } finally {
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    System.out.println("Error al cerrar el ResultSet");
+                    e.printStackTrace();
+                }
+            }
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException e) {
+                    System.out.println("Error al cerrar el Statement");
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
@@ -35,3 +85,4 @@ public class GestorBaseDatos {
     }
 
 }
+
